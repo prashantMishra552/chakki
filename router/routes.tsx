45db266ad2b34bd76a4e756e1988aa221cc46f5f
vaibhavsrivastava7777 <--global>
2023-05-14 15:40:21 +0530

    reac router

---
+++ router/routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../frontend/src/App/App';
+export const routes = [
+    {
+        path: "/",
+        Component() {
+            return <App />;
+        },
+    },
+];
+
